a1d3d4b3f5c765aebe3dc9d0a4a764dfa0d5d852
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-09-11 07:50:51 +0700

    remove unused ksp dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
     alias(libs.plugins.androidLibrary) apply false
 }
